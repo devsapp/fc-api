0d0f45a9cd9169355f027cd775a00b57d3da0d0d
wss-git <wssgryx@163.com>
2021-11-16 17:10:58 +0800

    build 0.0.46

--- lib/endpoint.d.ts
+++
@@ -1,8 +0,0 @@
-interface ICustomEndpoint {
-    fcEndpoint: string;
-    accountId: string;
-    region: string;
-    protocol: string;
-}
-export declare function getFcEndpoint(): Promise<ICustomEndpoint | undefined>;
-export {};
